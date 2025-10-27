e428db32645002cad3a3fcfdabb9d81ab37c4bc1
Gonzalez90 <sg119@my.tamuct.edu>
2025-10-27 10:38:15 -0500

    Update subjectClass.java

--- subjectClass.java
+++ subjectClass.java
@@ -1,1 +1,12 @@
+//Class stores data about a subject
 
+public class Subject
+  {
+    private String subjectName;      //Name of the subject
+    private Double subjectGrade;     //Holds grade
+
+    public Subject(String name, Double grade)
+    {
+      subjectName = name;
+      subjectGrade = grade;
+    }
